8214d7d519c9b15df959b7cdbf233859c2a054e4
Elena Justo <elena11j@protonmail.ch>
2026-04-30 13:09:06 +1000

    fix makefile using special characters. modify main.c to use structs to access registers. test flash on mcu

---
+++ ch6.1_peripheral_struct/main.c
@@ -0,0 +1,60 @@
+// Microcontroller: STM32G071R
+#include <stdint.h>
+
+// GPIO Struct
+typedef struct {
+	volatile uint32_t MODER; 	// Offset: 0x00
+	volatile uint32_t OTYPER; 	// Offset: 0x04
+	volatile uint32_t OSPEEDR; 	// Offset: 0x08
+	volatile uint32_t PUPDR; 	// Offset: 0x0C
+	volatile uint32_t IDR; 		// Offset: 0x10
+	volatile uint32_t ODR; 		// Offset: 0x14
+	volatile uint32_t BSRR;		// Offset: 0x18
+	volatile uint32_t LCKR; 	// Offset: 0x1C
+	volatile uint32_t AFRL; 	// Offset: 0x20
+	volatile uint32_t AFRH; 	// Offset: 0x24
+} GPIO_TypeDef;
+
+// RCC Struct
+typedef struct {
+	volatile uint32_t DUMMY[13]; // Padding to start at relevant offset
+	volatile uint32_t IOPENR;	 // Offset: 0x34
+} RCC_TypeDef;
+
+// Base addresses
+#define RCC_BASE	0x40021000	// For STM32G07
+#define	GPIOA_BASE	0x50000000	// For STM32G07
+
+// Peripheral pointer definitions
+#define RCC		((RCC_TypeDef*) RCC_BASE)
+#define GPIOA	((GPIO_TypeDef*) GPIOA_BASE)
+
+// Bit mask
+#define GPIOAEN		(1U<<0)		// Bit mask for GPIOA A bit 0
+#define PIN5		(1U<<5)		// Bit mask for GPIOA A pin 5
+#define LED_PIN		PIN5		// Alias for LED pin
+
+// Main program
+int main(void) {
+
+	RCC->IOPENR |= GPIOAEN; 	// Enable clock access to GPIOA
+
+	GPIOA->MODER |= (1U<<10); 	// Set bit 10 to 1
+	GPIOA->MODER &= ~(1U<<11);	// Set bit 11 to 0
+	
+	// Main loop
+	while(1) {
+
+		// Toggle PA5
+		GPIOA->ODR ^= LED_PIN;
+
+		// Delay
+		for (int i = 0; i < 500000; i++){}
+	}
+}
+
+
+
+
+
+
---
+++ ch6.1_peripheral_struct/stm32g0_startup.c
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+/*Symbols defined in the linker script */
+extern uint32_t _endstack;
+extern uint32_t _endtext;
+extern uint32_t _startdata;
+extern uint32_t _enddata;
+extern uint32_t _startbss;
+extern uint32_t _endbss;
+
+/* Function prototypes */
+void Reset_Handler(void);
+int main(void);
+
+/* Exception and Interrupt Handlers */
+void NMI_Handler(void)__attribute__((weak,alias("Default_Handler")));
+void HardFault_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+void MemManage_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
+
+/* Vector Table */
+uint32_t vector_tbl[] __attribute__((section(".isr_vector_tbl")))={
+	(uint32_t)&_endstack,
+	(uint32_t)&Reset_Handler,
+	(uint32_t)&NMI_Handler,
+	(uint32_t)&HardFault_Handler,
+	(uint32_t)&MemManage_Handler,
+};
+
+/* Default handler that enters an infinite loop */
+void Default_Handler(void)
+{
+	while(1){}
+}
+
+/* Reset Handler */
+void Reset_Handler(void)
+{
+	// Calculate the sizes of the .data and .bss sections
+	uint32_t data_mem_size =  (uint32_t)&_enddata - (uint32_t)&_startdata;
+	uint32_t bss_mem_size  =   (uint32_t)&_endbss - (uint32_t)&_startbss;
+
+	/* Convert byte sizes to 32-bit word counts to match uint32_t* copies (4 == sizeof(uint32_t)) */
+    data_mem_size /= 4;
+    bss_mem_size  /= 4;
+	
+	// Initialize pointers to the source and destination of the .data section
+	uint32_t *p_src_mem =  (uint32_t *)&_endtext;
+	uint32_t *p_dest_mem = (uint32_t *)&_startdata;
+	
+	/*Copy .data section from FLASH to SRAM*/
+	for(uint32_t i = 0; i < data_mem_size; i++  )
+	{
+		*p_dest_mem++ = *p_src_mem++;
+	}
+	
+	// Initialize the .bss section to zero in SRAM
+	p_dest_mem =  (uint32_t *)&_startbss;
+	
+	for(uint32_t i = 0; i < bss_mem_size; i++)
+	{
+		/*Set bss section to zero*/  
+		*p_dest_mem++ = 0;
+	}
+	
+	// Call the application's main function.
+	main();
+}
